e8d40556379d3289f66b5f74e5386809311e7447
melviii100 <melvinjose1992@gmail.com>
2017-09-25 05:02:17 +0000

    Done

--- build.py
+++ build.py
@@ -2,32 +2,40 @@ import pandas as pd
 
 
 def load_data():
-    """
-        Enter your code here
-    """
+    filename = "files/olympics.csv"
+    new_cols = ['Country_name_and_code', '# Summer', 'Gold', 'Silver', 'Bronze', 'Total', 'Winter', 'Gold', 'Silver', 'Bronze', 'Total.1', 'Games', 'Gold', 'Silver', 'Bronze', 'Combined total']
+    df = pd.read_csv(filename, skiprows=[0,1], names=new_cols, header=None)
+    #df.rename(columns={'01 !' : "Gold", '02 !': "Silver", '03 !' : "Bronze"}, inplace=True)
+    df.Country_name_and_code =  df.Country_name_and_code.str.replace("\xc2\xa0", "")
+    #only_country_names = [x[0].strip("\xc2\xa0") for x in df["Country_name_and_code"].str.split("(")]
+    only_country_names = [x[0] for x in df["Country_name_and_code"].str.split("(")]
+    df.index = only_country_names
+
+    #Method 1
+    #use_cols = [col for col in df.columns if col[:5] != "Total" ]
+    #return df[use_cols]
+
+    # If you want specified coulmns
+    #return df.filter(like="Total", axis=1)  # df.filter(regex="Total", axis=1)
+
+    return df[df.columns.drop(list(df.filter(regex="Total")))]
 
 
 def first_country(df):
-    """
-        Enter your code here
-    """
+    return df.iloc[0]
 
 
 def gold_medal(df):
-    """
-        Enter your code here
-    """
+    return df.iloc[:-1, [2]].idxmax()[0]
 
 
 def biggest_difference_in_gold_medal(df):
-    """
-        Enter your code here
-    """
+    return (df.iloc[:-1,2] -  df.iloc[:-1,6]).abs().idxmax()
+
 
 def get_points(df):
-    """
-    Enter your code here
-    """
+    df["Points"] = (df["Gold"] * 3) + (df['Silver'] * 2) + (df["Bronze"]) + (df["Gold.1"] * 3) + (df['Silver.1'] * 2) + (df["Bronze.1"])
+    return df["Points"]
 
 
 # df = load_data()
